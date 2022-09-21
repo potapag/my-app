344376b1223b5e08737615a0a420d165f17c50f0
andrey <potapag@gmail.com>
2022-09-21 16:25:24 +0300

    conditional rendering

--- src/components/counter.jsx
+++ src/components/counter.jsx
@@ -1,21 +1,67 @@
-import React from "react";
+import React, {useState} from "react";
 
 const Counter = () => {
-    const count = 0;
+    const [count, setCount] = useState(0);
+    const [tags, setTags] = useState(['tag1', 'tag2', 'tag3']);
+
     const formatCount = () => {
         return count === 0 ? "empty" : count;
     };
+
     const getBadeClasses = () => {
         let classes = "badge m-2 ";
         classes += count === 0 ? "bg-warning" : "bg-primary";
         return classes;
     }
 
+    const hemdleIncrement = () => {
+        setCount((prevState) => prevState + 1);
+    };
+
+    const hemdleDecrement = () => {
+        setCount((prevState) => prevState - 1);
+    };
+
+    const hendleTagChange = (id) => {
+        setTags(prevState => prevState.filter(tag => tag !== id));
+    };
+
+    const renderTags = () => {
+        return (
+            tags.length !== 0 &&
+            tags.map(tag => (
+                <li 
+                    key={tag} 
+                    className="btn btn-primary btn-sm m-2"
+                    onClick={() => hendleTagChange(tag)}
+                >
+                    {tag}
+                </li>
+            ))
+        )
+    };
+    if (tags.length !== 0) {
+        return <ul>
+                {renderTags()}
+            </ul>
+    }
+
     return (
-    <>
-        <span className={getBadeClasses()}>{formatCount()}</span>
-        <button className="btn btn-primary btn-sm m-2">+</button>
-    </>
+        <>
+            <span className={getBadeClasses()}>{formatCount()}</span>
+            <button 
+                className="btn btn-primary btn-sm m-2" 
+                onClick={hemdleIncrement}
+            >
+                +
+            </button>
+            <button 
+                className="btn btn-primary btn-sm m-2" 
+                onClick={hemdleDecrement}
+            >
+                -
+            </button>
+        </>
     );
 };
 
--- src/index.js
+++ src/index.js
@@ -1,23 +1,10 @@
 import React from "react";
 import reactDom from "react-dom";
-// import ReactDOM from "react-dom/client";
 import "bootstrap/dist/css/bootstrap.css";
 import Counter from "./components/counter";
-
-// React 17
-
 
 const App = () => {
     return <Counter />;
 };
 
 reactDom.render(<App />, document.getElementById("root"));
-
-// React 18
-// const root = ReactDOM.createRoot(document.getElementById("root"));
-
-// root.render(
-//             <React.StrictMode>
-//                 <h1>hello world</h1>
-//             </React.StrictMode>
-// );
