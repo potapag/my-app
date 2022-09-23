eab5739bac65a4b2b1771a972a8becec6c3cbbb2
andrey <potapag@gmail.com>
2022-09-23 10:17:44 +0300

    delete counter

--- src/components/countersList.jsx
+++ src/components/countersList.jsx
@@ -1,19 +1,30 @@
-import React from "react";
+import React, {useState} from "react";
 import Counter from "./counter";
 
 const CountersList = () => {
-    const counters = [
+    const [counters, setCounters] = useState([
         { id: 0, value: 0, name: "Вещь" },
         { id: 1, value: 4, name: "Ложка" },
         { id: 2, value: 0, name: "Вилка" },
         { id: 3, value: 0, name: "Тарелка" },
         { id: 4, value: 0, name: "Набор" },
-    ];
+    ]);
+
+    const handleDelete = (id) => {
+        const newCounters = counters.filter(c => c.id !== id);
+        setCounters(newCounters);
+    };
 
     return (
         <>
             {counters.map(count => (
-                <Counter key={count.id} value={count.value} name={count.name} />
+                <Counter 
+                    key={count.id}
+                    id={count.id}
+                    value={count.value}
+                    name={count.name}
+                    onDelete={handleDelete}
+                />
             ))}
         </>
     );
